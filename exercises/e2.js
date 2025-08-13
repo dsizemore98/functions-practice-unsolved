9f1a8c6b78552b3e62afd8d98aef13035b1b0aff
dsizemore98 <dsizemore1098@gmail.com>
2025-08-13 18:59:20 -0400

    Update e2.js

--- exercises/e2.js
+++ exercises/e2.js
@@ -12,12 +12,29 @@
 
 // Your function code starts here...
 
+function logMyInfo(name, age){
+  console.log("My name is " + name +". I am " + age + " years old!");
+}
+
+logMyInfo('Andrey', 18);
+
+
 
 // Use the function you created above to calculate the areas of 3 rooms
 // Assign the results to room1, room2, and room3 variables
 // Data for calculation: room1: 4 by 6, room2: 13 by 3, room3: 4 by 8
 
 // Your code goes here:
+
+function rectArea(sideA, sideB) {
+  return sideA * sideB;
+}
+
+let room1 = rectArea(4, 6);
+let room2 = rectArea(13, 3);
+let room3 = rectArea(4, 9);
+
+
 
 
 
@@ -33,13 +50,18 @@
 
 // Your code goes here...
 
+function totalRoomsArea(area1, area2, area3){
+  return area1 + area2 + area3;
+}
 
 
 // Make the function call in the parentheses of the console.log statement.
 // Use room1, room2, room3 variables as arguments for the function call
 const result = function () {
-    console.log(/* replace this comment with a function call here... */);
+    console.log(totalRoomsArea(room1, room2, room3));
 }
+
+result();
 
 
 
